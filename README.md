2a6c0558f0b3b3dc257a5a7dde3ccfe8c30d28ab
srijanani18 <100787221+srijanani18@users.noreply.github.com>
2023-07-06 21:51:25 +0530

    upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Digital_library_
+ 
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                   &copy;Online Library Management System 
+                </div>
+
+            </div>
+        </div>
+    </section>
